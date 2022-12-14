920cc3e06de2014860ad1a15ecee67fc8acd1eea
dee acharjya <deeAcharjya@gamestop.com>
2022-12-14 07:44:48 -0700

    Inital commit

---
+++ FixWithCustomSerialization/Controllers/MediaController.cs
@@ -0,0 +1,70 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace FixWithCustomSerialization.Controllers;
+
+[ApiController]
+[Route("[controller]")]
+public class MediaController : ControllerBase
+{
+    private readonly ILogger<MediaController> _logger;
+
+    public MediaController(ILogger<MediaController> logger)
+    {
+        _logger = logger;
+    }
+
+    [HttpPost]
+    public MediaFile UpdateMedia([FromBody] MediaFile media)
+    {
+        throw new NotImplementedException();
+
+        /* todo: Implement the method
+         * 
+         * 1. media will be contained in the ImageDataB64 prop. It should have been already saved to the webroots folder by the "read" method of MediaFileJsonConverter
+         * 2. Save the media object in Mongo db
+         * 3. return the MediaFile object
+         * 4. We expect the "write" method of MediaFileJsonConverter to create the public URL
+         * 
+         * */
+    }
+
+    [HttpGet("{Name}")]
+    public MediaFile GetMedia(string Name)
+    {
+        throw new NotImplementedException();
+
+        /*
+         * return _mongoDb.getcollection<MediaFile>.Find(m=>m.Name==Name).SingleAsync();
+         * 
+         * 1. We expect the "write" method of MediaFileJsonConverter to create the public URL
+         * 
+         */
+    }
+}
+
+/// <summary>
+/// todo: implement MediaFileJsonConverter  
+/// [JsonConverter(typeof(MediaFileJsonConverter))]
+/// </summary>
+public class MediaFile
+{
+    /// <summary>
+    /// todo: We want media name to be Unique, Please enforce using MongoDb Unique Index
+    /// </summary>
+    public string Name { get; set; } = "";
+
+    /// <summary>
+    /// todo: donot Save in database
+    /// </summary>
+    public string ImageDataB64 { get; set; } = "";
+
+    /// <summary>
+    /// todo: donot Save in database, Generate dynamically using 
+    /// </summary>
+    public string PublicUrl { get; set; } = "";
+
+
+}
+
+
+
--- README.md
+++ README.md
@@ -1,2 +1,8 @@
 # net-core-challenge-1
-simple dot net core code challenge to check knowledge of asp dot net core
+This is starter project for simple code challenge
+
+# goal
+Please FORK this repo and send it my way
+1. Please search for todo: and implemetent the MediaController Methods
+2. Implement the MediaFileJsonConverter custome JSON Controller.
+3. Here's some documents that can get you upto speed https://learn.microsoft.com/en-us/dotnet/standard/serialization/system-text-json/converters-how-to?pivots=dotnet-6-0
